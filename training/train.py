9297217ad1ff7436e9fa87c10cab4641424e5098
cveta <cvetacapova@yahoo.com>
2025-01-15 08:26:03 +0100

    Added ProjectConfiguration for Accelerator

--- training/train.py
+++ training/train.py
@@ -1,4 +1,5 @@
 import torch
+from accelerate.utils import ProjectConfiguration
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from accelerate import Accelerator
 from datasets import Dataset
@@ -9,7 +10,7 @@ import os
 import json
 import math
 
-resume = True
+resume = False
 
 # only for mac, remove for training on cuda or cpu
 if torch.backends.mps.is_available():
@@ -31,7 +32,15 @@ tokenizer = Tokenizer.from_file('tokenizer/bpe.json')
 
 train_dlp = DataloaderProvider(train_dataset, config.BATCH_SIZE, tokenizer)
 
-accelerator = Accelerator(project_dir="accelerator")
+output_dir = "accelerator_checkpoints"
+accelerator_project_config = ProjectConfiguration(
+    total_limit=3,
+    automatic_checkpoint_naming=True,
+    project_dir=output_dir,
+    logging_dir=os.path.join(output_dir, 'logs'),
+)
+
+accelerator = Accelerator(project_config=accelerator_project_config)
 
 print("Computing positional encodings...")
 
@@ -66,32 +75,29 @@ accelerator.register_for_checkpointing(sheduler)
 accelerator.register_for_checkpointing(model)
 accelerator.register_for_checkpointing(optimizer)
 
-output_dir = "accelerator_checkpoint_batch_10"
+output_dir = "accelerator_checkpoints"
+last_batch = 0 #use last batch to continue training from that point
 if resume:
     print("Loading accelerator state...")
-    accelerator.load_state(output_dir)
+    accelerator.load_state(output_dir+"/checkpoints/checkpoint_6")
+    with open(f"{output_dir}/metadata.json", "r") as f:
+        metadata = json.load(f)
+    last_batch = metadata["batch"]
+    skipped_dataloader = accelerator.skip_first_batches(train_dataloader, last_batch)
+
 
 if not resume:
     print("Saving initial accelerator state...")
-    accelerator.save_state(output_dir="accelerator")
+    accelerator.save_state()
 
 
 def train_epoch(model, train_dataloader, optimizer, loss_function, sheduler, accelerator):
     model.train()
     train_loss = 0
-    num_batches = 0
-    last_batch = 0
+    num_batches = last_batch
 
-    if resume:
-        with open(f"{output_dir}/metadata.json", "r") as f:
-            metadata = json.load(f)
-        last_batch = metadata["batch"] #use last batch to continue training from that point
-        num_batches = last_batch
 
     for batch in train_dataloader:
-        if last_batch > 0:
-            last_batch = last_batch - 1 #skip the batches that have already been trained
-            continue
 
         sources = batch['sources']
         targets = batch['targets']
@@ -110,12 +116,11 @@ def train_epoch(model, train_dataloader, optimizer, loss_function, sheduler, acc
         train_loss += loss.item()
         num_batches += 1
 
-        accelerator.save_state(output_dir="accelerator")
 
         if num_batches % 10 == 0:
-            checkpoint_dir = f"accelerator_checkpoint_batch_{num_batches}"
+            checkpoint_dir = f"accelerator_checkpoints"
             print(f"Saving checkpoint to {checkpoint_dir}...")
-            accelerator.save_state(output_dir=checkpoint_dir)
+            accelerator.save_state()
             # Save progress
             metadata = {
                 "batch": num_batches,
@@ -123,7 +128,7 @@ def train_epoch(model, train_dataloader, optimizer, loss_function, sheduler, acc
             with open(f"{checkpoint_dir}/metadata.json", "w") as f:
                 json.dump(metadata, f)
 
-        if num_batches == 100:
+        if num_batches == 200:
             break
         sheduler.step(loss.item())
 
